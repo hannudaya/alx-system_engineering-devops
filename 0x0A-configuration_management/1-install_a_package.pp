61702f31f0a4592a341abb215eacb59100166f37
hannudaya <hannudaya@github.com>
2023-03-24 13:35:13 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # install flask -v 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+exec { 'flask':
+  command => '/usr/bin/apt-get -y pip3 install flask -v 2.1.0',
 }
